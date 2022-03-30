4065ac758ee438d195b389db5b452616d28eb697
Code Sandbox <hello@codesandbox.io>
2022-03-30 03:28:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.91fb5a0234cf2f9eca5759bc8448ca06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df839067fb0323f9326e116444381a0",
+    "url": "https://anilsakr.github.io/csb-pujfx0/index.html"
+  },
+  {
+    "revision": "d7b07635d7e4eec64558",
+    "url": "https://anilsakr.github.io/csb-pujfx0/static/css/main.e72d4031.chunk.css"
+  },
+  {
+    "revision": "86b35f52e2d1af03f168",
+    "url": "https://anilsakr.github.io/csb-pujfx0/static/js/2.ff345028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://anilsakr.github.io/csb-pujfx0/static/js/2.ff345028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b07635d7e4eec64558",
+    "url": "https://anilsakr.github.io/csb-pujfx0/static/js/main.8c6f8ae8.chunk.js"
+  },
+  {
+    "revision": "214ab18cad563f7920ac",
+    "url": "https://anilsakr.github.io/csb-pujfx0/static/js/runtime-main.bb6e8d00.js"
+  }
+]);
